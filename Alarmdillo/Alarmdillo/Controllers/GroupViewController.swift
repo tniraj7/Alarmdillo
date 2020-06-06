002cb024e6e17aa1fe37c9614c0e555a79b1e5ae
Niraj Tiwari <tniraj7@gmail.com>
2020-06-06 23:09:45 +0900

    add methods  to deque different tpyes of cell and configure them

--- Alarmdillo/Alarmdillo/Controllers/GroupViewController.swift
+++ Alarmdillo/Alarmdillo/Controllers/GroupViewController.swift
@@ -4,11 +4,22 @@ class GroupViewController: UITableViewController {
 
     var group: Group!
     
+    let playSoundTag = 1001
+    let enabledTag = 1002
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavBar()
     }
     
-    @IBAction func switchChanged(_ sender: Any) {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    private func configureNavBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addAlarm))
+        title = group.name
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -35,6 +46,52 @@ class GroupViewController: UITableViewController {
         }
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            return createGroupCell(for: indexPath, in: tableView)
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RightDetail", for: indexPath)
+            let alarm = group.alarms[indexPath.row]
+            cell.textLabel?.text = alarm.name
+            cell.detailTextLabel?.text = DateFormatter.localizedString(from: alarm.time, dateStyle: .none, timeStyle: .short)
+            return cell
+        }
+    }
+    
+    private func createGroupCell(for indexPath: IndexPath, in tableView: UITableView) -> UITableViewCell {
+        
+        switch(indexPath.row) {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "EditableText", for: indexPath)
+            
+            if let cellTextField = cell.viewWithTag(1) as? UITextField {
+                cellTextField.text = group.name
+            }
+            return cell
+            
+        case 1:
+             let cell = tableView.dequeueReusableCell(withIdentifier: "Switch", for: indexPath)
+            
+             if let cellLabel = cell.viewWithTag(1) as? UILabel, let cellSwitch = cell.viewWithTag(2) as? UISwitch {
+                cellLabel.text = "Play Sound"
+                cellSwitch.isOn = group.playSound
+                cellSwitch.tag = playSoundTag
+            }
+             return cell
+            
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Switch", for: indexPath)
+            
+            if let cellLabel = cell.viewWithTag(1) as? UILabel, let cellSwitch = cell.viewWithTag(2) as? UISwitch {
+                cellLabel.text = "Enabled"
+                cellSwitch.isOn = group.enabled
+                cellSwitch.tag = enabledTag
+            }
+            return cell
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return indexPath.section == 1
     }
@@ -43,6 +100,41 @@ class GroupViewController: UITableViewController {
         
         group.alarms.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .automatic )
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.preservesSuperviewLayoutMargins = false
+        cell.contentView.preservesSuperviewLayoutMargins = false
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        
+        if sender.tag == playSoundTag {
+            group.playSound = sender.isOn
+        } else {
+            group.enabled = sender.isOn
+        }
+    }
+    
+    @objc private func addAlarm() {
+        let newAlarm = Alarm(name: "Name this Alarm", caption: "This is description", time: Date(), image: "")
+        group.alarms.append(newAlarm)
+        performSegue(withIdentifier: "EditAlarm", sender: newAlarm)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let alarmToEdit: Alarm
+        
+        if sender is Alarm {
+            alarmToEdit = sender as! Alarm
+        } else {
+            guard let selectedIndexPath = tableView.indexPathForSelectedRow else { return }
+            alarmToEdit = group.alarms[selectedIndexPath.row]
+        }
+        
+        if let alarmViewController = segue.destination as? AlarmViewController {
+            alarmViewController.alarm = alarmToEdit
+        }
     }
 }
 
