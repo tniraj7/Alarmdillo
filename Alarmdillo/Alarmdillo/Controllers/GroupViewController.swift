41b178e3803d4510ec959a98fc8675a30220b842
Niraj Tiwari <tniraj7@gmail.com>
2020-06-06 22:16:34 +0900

    add tableview delegate callbacks for different sections of GroupViewController

--- Alarmdillo/Alarmdillo/Controllers/GroupViewController.swift
+++ Alarmdillo/Alarmdillo/Controllers/GroupViewController.swift
@@ -11,4 +11,37 @@ class GroupViewController: UITableViewController {
     @IBAction func switchChanged(_ sender: Any) {
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return nil
+        }
+        
+        if group.alarms.count > 0 {
+            return "Alarms"
+        }
+        
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 3
+        } else {
+            return group.alarms.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.section == 1
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        group.alarms.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic )
+    }
 }
