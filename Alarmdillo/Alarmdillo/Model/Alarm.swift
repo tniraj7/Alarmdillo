15159b37bb09861be424bccd69d042a676f68d97
Niraj Tiwari <tniraj7@gmail.com>
2020-06-09 23:32:11 +0900

    conform the models to NSCoder protocol and implement stubs

--- Alarmdillo/Alarmdillo/Model/Alarm.swift
+++ Alarmdillo/Alarmdillo/Model/Alarm.swift
@@ -1,12 +1,28 @@
 import UIKit
 
-class Alarm: NSObject {
-
-    var id: String
-    var name: String
-    var caption: String
-    var time: Date
-    var image: String
+class Alarm: NSObject, NSCoding {
+    
+    var id      : String
+    var name    : String
+    var caption : String
+    var time    : Date
+    var image   : String
+    
+    required init?(coder: NSCoder) {
+        self.id = coder.decodeObject(forKey: "id") as! String
+        self.name = coder.decodeObject(forKey: "name") as! String
+        self.caption = coder.decodeObject(forKey: "caption") as! String
+        self.time = coder.decodeObject(forKey: "time") as! Date
+        self.image = coder.decodeObject(forKey: "image") as! String
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(self.id, forKey: "id")
+        coder.encode(self.name, forKey: "name")
+        coder.encode(self.caption, forKey: "caption")
+        coder.encode(self.time, forKey: "time")
+        coder.encode(self.image, forKey: "image")
+    }
     
     init(name: String, caption: String, time: Date, image: String) {
         self.id = UUID().uuidString
--- Alarmdillo/Alarmdillo/Model/Group.swift
+++ Alarmdillo/Alarmdillo/Model/Group.swift
@@ -1,12 +1,28 @@
 import UIKit
 
-class Group: NSObject {
-
+class Group: NSObject, NSCoding {
+    
     var id        : String
     var name      : String
     var playSound : Bool
-    var enabled    : Bool
+    var enabled   : Bool
     var alarms    : [Alarm]
+    
+    required init?(coder: NSCoder) {
+        self.id = coder.decodeObject(forKey: "id") as! String
+        self.name = coder.decodeObject(forKey: "name") as! String
+        self.playSound = coder.decodeObject(forKey: "playSound") as! Bool
+        self.enabled = coder.decodeObject(forKey: "enabled") as! Bool
+        self.alarms = coder.decodeObject(forKey: "alarms") as! [Alarm]
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(self.id, forKey: "id")
+        coder.encode(self.name, forKey: "name")
+        coder.encode(self.playSound, forKey: "playSound")
+        coder.encode(self.enabled, forKey: "enabled")
+        coder.encode(self.alarms, forKey: "alarms")
+    }
     
     init(name: String, playSound : Bool, enable: Bool, alarms: [Alarm]) {
         self.id        = UUID().uuidString
